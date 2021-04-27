45a9c18267c087ad88a78fbebe7a75f3d7bfce63
PAMPACOMPASSO\marina.silva <marina.silva@compasso.com.br>
2021-04-27 09:41:57 -0300

    First test comment

--- cypress/integration/ticketbox.spec.js
+++ cypress/integration/ticketbox.spec.js
@@ -1,6 +1,6 @@
 describe('Ticketbox', () => {
     beforeEach(() => cy.visit('index.html'));
-  
+    //First Test
     it('Checks for the initial state', () => {
       cy.percySnapshot();
     });
